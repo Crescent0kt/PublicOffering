aa7afd4e81217c19f63850346cab193a689e36e6
okt03149 <wwwkj1232@gmail.com>
2024-04-14 15:51:53 +0900

    "init Project"

---
+++ src/main/java/com/example/publicoffering/controller/MainPageController.java
@@ -0,0 +1,20 @@
+package com.example.publicoffering.controller;
+
+import com.example.publicoffering.service.MainService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+
+    private final MainService mainService;
+
+    public MainPageController(MainService mainService) {
+        this.mainService = mainService;
+    }
+
+    @GetMapping("/healthCheck")
+    public String healthCheck(){
+        return "Health";
+    }
+}
