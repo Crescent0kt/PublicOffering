6c11c2ad90f29859bfc13f1b80495b63b0d98cc7
Crescent0kt <gyungtaek0@gmail.com>
2024-04-15 12:53:37 +0900

    feat: 크롤링 후 출력

--- src/main/java/com/example/publicoffering/PublicOfferingApplication.java
+++ src/main/java/com/example/publicoffering/PublicOfferingApplication.java
@@ -3,7 +3,6 @@ package com.example.publicoffering;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 @SpringBootApplication
 @EnableScheduling
 public class PublicOfferingApplication {
--- src/main/java/com/example/publicoffering/scheduler/CrawlingScheduler.java
+++ src/main/java/com/example/publicoffering/scheduler/CrawlingScheduler.java
@@ -1,13 +1,37 @@
 package com.example.publicoffering.scheduler;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CrawlingScheduler {
+    private final String publicOfferingUrl = "http://www.ipostock.co.kr/sub03/ipo04.asp";
     @Scheduled(cron = "1 * * * * *")
     public void Crawling(){
-        System.out.println("test");
+        try{
+            Document doc = Jsoup.connect(publicOfferingUrl).get();
+            Element tableElement = doc.selectXpath("//*[@id='print']/table[1]/tbody/tr[4]/td/table/tbody/tr[4]/td/table").first();
+            assert tableElement != null;
+            Elements rows = tableElement.select("tr");
+
+            for (Element row : rows) {
+                Elements cells = row.select("td");
+                for (Element cell : cells) {
+
+                    System.out.println(cell.text());
+                }
+            }
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
 
     }
 }
