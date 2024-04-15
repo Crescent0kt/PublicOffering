df89ec298480ffc037c79cd19de0fc3d607acd77
Crescent0kt <gyungtaek0@gmail.com>
2024-04-15 13:08:11 +0900

    feat: 크롤링 후 출력

--- src/main/java/com/example/publicoffering/scheduler/CrawlingScheduler.java
+++ src/main/java/com/example/publicoffering/scheduler/CrawlingScheduler.java
@@ -1,5 +1,6 @@
 package com.example.publicoffering.scheduler;
 
+import lombok.Data;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,21 +8,51 @@ import org.jsoup.select.Elements;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+class PublicOffer{
+    String scheduleDate;
+    String itemName;
+    String desiredPrice;
+    String offeringPrice;
+    String offeringAmount;
+    String refundDate;
+    String listDate;
+    String competitionRatio;
+    String leadManager;
+
+
+}
 @Component
 public class CrawlingScheduler {
-    private final String publicOfferingUrl = "http://www.ipostock.co.kr/sub03/ipo04.asp";
+
+
     @Scheduled(cron = "1 * * * * *")
-    public void Crawling(){
+    public void method(){
+        List<PublicOffer> publicOfferList = Crawling();
+    }
+
+    private List<PublicOffer> Crawling(){
+        List<PublicOffer> publicOfferList = new ArrayList<>();
         try{
+            String publicOfferingUrl = "http://www.ipostock.co.kr/sub03/ipo04.asp";
             Document doc = Jsoup.connect(publicOfferingUrl).get();
             Element tableElement = doc.selectXpath("//*[@id='print']/table[1]/tbody/tr[4]/td/table/tbody/tr[4]/td/table").first();
-            assert tableElement != null;
-            Elements rows = tableElement.select("tr");
 
+            if(tableElement == null){
+                return null;
+            }
+
+            Elements rows = tableElement.select("tr");
             for (Element row : rows) {
                 Elements cells = row.select("td");
-                for (Element cell : cells) {
+                if (cells.size() > 1){
+                    PublicOffer publicOffer = new PublicOffer();
 
+                }
+                for (Element cell : cells) {
                     System.out.println(cell.text());
                 }
             }
@@ -31,7 +62,7 @@ public class CrawlingScheduler {
             System.out.println(e.getMessage());
         }
 
-
+        return publicOfferList;
 
     }
 }
